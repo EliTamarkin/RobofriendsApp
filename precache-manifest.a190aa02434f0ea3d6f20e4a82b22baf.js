7cedef9c42e0adb6154978a82982319ac086a4a9
EliTamarkin <elitamarkin98@gmail.com>
2019-10-20 21:57:37 +0300

    Updates

---
+++ precache-manifest.a190aa02434f0ea3d6f20e4a82b22baf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8ba4fc67f16c3ef6767c6ba9301dc34",
+    "url": "/RobofriendsApp/index.html"
+  },
+  {
+    "revision": "7b3f358e1e5a4b4c664d",
+    "url": "/RobofriendsApp/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "f2b69a60fcee9fa4563e",
+    "url": "/RobofriendsApp/static/css/main.607eab5d.chunk.css"
+  },
+  {
+    "revision": "7b3f358e1e5a4b4c664d",
+    "url": "/RobofriendsApp/static/js/2.782ad8cb.chunk.js"
+  },
+  {
+    "revision": "f2b69a60fcee9fa4563e",
+    "url": "/RobofriendsApp/static/js/main.1b03fa92.chunk.js"
+  },
+  {
+    "revision": "a5be5d6faa0f5566369e",
+    "url": "/RobofriendsApp/static/js/runtime-main.5d05d7ac.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RobofriendsApp/static/media/SEGA.12a3db8b.woff"
+  }
+]);
