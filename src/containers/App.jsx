6ac93b993e60f20fdffeb5239a017c0aaee8fe96
EliTamarkin <elitamarkin98@gmail.com>
2019-11-02 23:08:41 +0200

    added redux

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -5,34 +5,42 @@ import Scroll from "../components/Scroll.jsx";
 import ErrorBoundry from "../components/ErrorBoundry.jsx";
 import "./App.css";
 
-class App extends React.Component{
-constructor(){
-    super();
-    this.state ={
-        robots: [],
-        searchfield: ""
+import {connect} from "react-redux";
+import {setSearchField, requestRobots} from "../actions.js";
+
+const mapStateToProps = (state) => {
+    return {
+        searchField : state.searchRobots.searchField,
+        isPending: state.requestRobots.isPending,
+        robots: state.requestRobots.robots,
+        error: state.requestRobots.error,
     }
 }
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRobotsRequest: () => dispatch(requestRobots())
+    }
+}
+
+class App extends React.Component{
+
 componentDidMount(){
-    fetch("https://jsonplaceholder.typicode.com/users")
-    .then(response => response.json())
-    .then(users => this.setState({
-        robots: users
-    }))
+ this.props.onRobotsRequest();
 }
- onSearchBarChange = (event) => {
-    this.setState({
-        searchfield : event.target.value
-    })
-}
+
 render(){
-    const matchingRobots = this.state.robots.filter(robot => {
-        return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    const {searchField, isPending, robots, onSearchChange} = this.props;
+    const matchingRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
-    return (
+    return isPending ?
+    <h1>Loading...</h1> :
+    (
         <div className="tc">
             <h1 className="f1">Robofriends</h1>
-            <SearchBox barChange = {this.onSearchBarChange} />
+            <SearchBox barChange = {onSearchChange} />
             <br />
             <Scroll>
                 <ErrorBoundry>
@@ -50,4 +58,4 @@ render(){
 
 
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
