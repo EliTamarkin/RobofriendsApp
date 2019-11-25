987dfdb7c0ebf7999615b82b311b3ae10748d7f1
EliTamarkin <elitamarkin98@gmail.com>
2019-11-25 22:05:05 +0200

    pwa changes

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const SearchBox = (props) => {
 return (
     <div>
-        <input type="text" name="searchfield" placeholder="search robots" onChange = {props.barChange} className="pa3 ba b--green bg-lightest-blue" />
+        <input aria-label="search-robots" type="text" name="searchfield" placeholder="search robots" onChange = {props.barChange} className="pa3 ba b--green bg-lightest-blue" />
     </div>
 )
 };
